fb2dfe5589268a1616631393022a0c887fb14c49
Ellis Clayton <ellis@ellisclayton.info>
2015-10-17 21:33:59 +1000

    Defined AngularJS App, basic dependencies and frameworks

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          livereload: true
+        }
+      }
+    },
+    // Compile Sass with compass into css/ directory
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'scss',
+          cssDir: 'css',
+          environment: 'production'
+        }
+      },
+    },
+    // Watch
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: 'scss/**/*.scss',
+        tasks: ['compass']
+      }
+    }
+  });
+
+  // Start web server
+  grunt.registerTask('serve', [
+    'connect:server',
+    'watch'
+  ]);
+
+  // Plugin loads
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['compass']);
+};
---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app', ['ngRoute', 'appControllers']);
+var appControllers = angular.module('appControllers', []);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
